c63ba4c513f3a66cd14a9636cc3882eb2433f0dd
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-06-22 21:33:55 +0700

    change id to uuid primary key and field is_email_verified

--- Users/models.py
+++ Users/models.py
@@ -1,4 +1,4 @@
-from typing import Optional
+import uuid
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import UserManager,PermissionsMixin
@@ -16,7 +16,7 @@ class UserProfilesManager(UserManager):
 
 # Create your models here
 class UserProfiles(AbstractBaseUser,PermissionsMixin):
-    
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     username = models.CharField(max_length=255)
     email = models.EmailField(max_length=255,unique=True)
     profile = models.ImageField(upload_to='profile',blank=True)
@@ -24,6 +24,7 @@ class UserProfiles(AbstractBaseUser,PermissionsMixin):
     role = models.CharField(max_length=10,choices=ROLE_CHOICES,default='User')
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
     objects = UserProfilesManager()
     
     USERNAME_FIELD = 'email'
