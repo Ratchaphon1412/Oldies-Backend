fb4fac9f4bff7d023383e6b0578d3d32b6f93b3d
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-06-26 00:44:55 +0700

    install request and remove firebase

--- Users/tests.py
+++ Users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class UserAPIVIewTest(TestCase):
+    
+    def test_signup_api_view_password_validate(self):
+        url = reversed('users:register')
+        body = {
+            "username":"ratchaphon1412",
+            "email":"test456@gmail.com",
+            "password":"111"  
+            }
+        response = self.client.post(url,body,format='json')
+        self.assertEqual(response.status_code,400)
