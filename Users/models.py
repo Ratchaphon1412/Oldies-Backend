46dd5ae338636d08ce9026af0ae1b6e4ca59264f
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-07-08 22:45:44 +0700

    add field in models users

--- Users/models.py
+++ Users/models.py
@@ -1,12 +1,13 @@
 import uuid
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import UserManager,PermissionsMixin
+from django.contrib.auth.models import UserManager, PermissionsMixin
+from django.db import models
 
 
-ROLE_CHOICES =(
-    ('User','User'),
-    ('Provider','Provider')
+ROLE_CHOICES = (
+    ('User', 'User'),
+    ('Provider', 'Provider')
 )
 
 SERVICE_CHOICES = (
@@ -17,27 +18,68 @@ SERVICE_CHOICES = (
 
 
 class UserProfilesManager(UserManager):
-    def get_by_natural_key(self, username: str | None) :
+    def get_by_natural_key(self, username: str | None):
         return super().get_by_natural_key(username)
 
 
 # Create your models here
-class UserProfiles(AbstractBaseUser,PermissionsMixin):
-    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+class UserProfiles(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255,unique=True)
-    profile = models.URLField(max_length=255,blank=True)
+    email = models.EmailField(max_length=255, unique=True)
+    profile = models.URLField(max_length=255, blank=True)
     password = models.CharField(max_length=255, blank=True, null=True)
-    role = models.CharField(max_length=10,choices=ROLE_CHOICES,default='User')
-    service = models.CharField(max_length=10,choices=SERVICE_CHOICES,default='Local')
+    role = models.CharField(
+        max_length=10, choices=ROLE_CHOICES, default='User')
+    service = models.CharField(
+        max_length=10, choices=SERVICE_CHOICES, default='Local')
+    phone = models.CharField(max_length=10, blank=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_email_verified = models.BooleanField(default=False)
     objects = UserProfilesManager()
-    
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    
-    
-    
-    
+
+
+class Country(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=255, blank=True, null=True)
+
+
+class Address(models.Model):
+    id = models.AutoField(primary_key=True)
+    unitNumber = models.TextField(max_length=255, blank=True, null=True)
+    streetNumber = models.TextField(max_length=255, blank=True, null=True)
+    address_line_1 = models.TextField(max_length=255, blank=True, null=True)
+    address_line_2 = models.TextField(max_length=255, blank=True, null=True)
+    city = models.TextField(max_length=255, blank=True, null=True)
+    region = models.TextField(max_length=255, blank=True, null=True)
+    postal_code = models.TextField(max_length=255, blank=True, null=True)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+
+
+class UserAddress(models.Model):
+    userID = models.ForeignKey(
+        UserProfiles, on_delete=models.SET_NULL, null=True)
+    addressID = models.ForeignKey(
+        Address, on_delete=models.SET_NULL, null=True)
+    is_default = models.BooleanField(default=False)
+
+
+class PaymentType(models.Model):
+    id = models.AutoField(primary_key=True)
+    value = models.CharField(max_length=255, blank=True, null=True)
+
+
+class UserPayment(models.Model):
+    id = models.AutoField(primary_key=True)
+    userID = models.ForeignKey(
+        UserProfiles, on_delete=models.SET_NULL, null=True)
+    paymentTypeID = models.ForeignKey(
+        PaymentType, on_delete=models.SET_NULL, null=True)
+    provider = models.CharField(max_length=255, blank=True, null=True)
+    account_number = models.CharField(max_length=255, blank=True, null=True)
+    expiry_date = models.DateField(blank=True, null=True)
+    is_default = models.BooleanField(default=False)
